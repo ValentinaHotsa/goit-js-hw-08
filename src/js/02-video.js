56fdfe5d9ffb2cfbc2feaf72e62f39835d752d55
ValentinaHotsa <134721626+ValentinaHotsa@users.noreply.github.com>
2023-10-04 22:34:11 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import _ from 'lodash';
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+    player.on('timeupdate', function(data) {
+   
+   player.getCurrentTime().then(function(seconds) {
+       console.log(seconds);
+       localStorage.setItem("videoplayer-current-time", seconds);
+})
+    });
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
