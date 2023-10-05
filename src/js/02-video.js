4825a9f0802529d4edfb8c18c36980cf034340f6
ValentinaHotsa <134721626+ValentinaHotsa@users.noreply.github.com>
2023-10-05 13:54:48 +0200

    task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,67 @@
 import Player from '@vimeo/player';
-import _ from 'lodash';
+import _, { throttle } from 'lodash';
 const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
+const player = new Player(iframe);
 
-    player.on('play', function() {
+player.on('play', function() {
         console.log('played the video!');
     });
 
-    player.getVideoTitle().then(function(title) {
+player.getVideoTitle().then(function(title) {
         console.log('title:', title);
     });
 
-    player.on('timeupdate', function(data) {
-   
-   player.getCurrentTime().then(function(seconds) {
-       console.log(seconds);
-       localStorage.setItem("videoplayer-current-time", seconds);
-})
-    });
+player.on('timeupdate', _.throttle(function (data) {
+    
+  
+    player.getCurrentTime().then(function (seconds) {
+        console.log(seconds);
+        localStorage.setItem("videoplayer-current-time", seconds);
+       
+    })
+
+     
+},  1000));
+
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
+
+
+
+
+
+// player.on("timeupdate", throttle(() => {
+   
+// }, 1000));
+
+// throttle(
+//     () => {
+
+//     }
+// ), 
+
+// player.on('timeupdate', function () {
+//  player._throttle("timeupdate", 1000)
+// })
+
+
+
+//  iframe._throttle("timeupdate",1000);
+
+//  player.on._throttle("timeupdate",1000);
+
+
+// // Avoid excessively updating the position while scrolling.
+// jQuery(window).on('scroll', _.throttle(updatePosition, 100));
+ 
+// // Invoke `renewToken` when the click event is fired, but not more than once every 5 minutes.
+// var throttled = _.throttle(renewToken, 300000, { 'trailing': false });
+// jQuery(element).on('click', throttled);
+ 
+// // Cancel the trailing throttled invocation.
+// jQuery(window).on('popstate', throttled.cancel);
+
+//      ._throttle(() => {
+//     console.log("Scroll handler call every 300ms");
+//   }, 300);
+    
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from "lodash.throttle"; 
+
+
+const form = document.querySelector(".feedback-form");
+const emailInput = form.querySelector('input');
+const messageInput = form.querySelector('textarea');
+
+
+
+function foo() {
+const email = form.elements.email.value;
+const message = form.elements.message.value;
+
+const obj = {
+    [form.elements.email.name]: email,
+    [form.elements.message.name]: message,
+};
+    console.log(obj);
+    localStorage.setItem('feedback-form-state', JSON.stringify(obj));
+}
+
+
+form.addEventListener('input', throttle(foo, 500));
+
+
+const getI = localStorage.getItem('feedback-form-state');
+// const parsedI = JSON.parse(getI);
+// console.log(parsedI);
+
+if (getI !== null) {
+    const parsedI = JSON.parse(getI);
+    console.log(parsedI);
+    emailInput.value = parsedI.email;
+    messageInput.value = parsedI.message;
+}
+
+
+function submitFunction(event) {
+    localStorage.removeItem("feedback-form-state");
+ };
+
+form.addEventListener('submit', submitFunction);
+
 
